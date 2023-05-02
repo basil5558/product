c27b8a9cfc24ba8052be2ca1edbdfde907e27b46
basil5558 <108655862+basil5558@users.noreply.github.com>
2023-05-02 17:19:08 +0530

    product for e-cart

---
+++ src/main/java/com/shoppingportal/product/entity/Product.java
@@ -0,0 +1,29 @@
+package com.shoppingportal.product.entity;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer productId;
+	private String name;
+	private Integer quantity;
+	private Integer price;
+	
+	
+}
